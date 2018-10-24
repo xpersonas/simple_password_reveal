5b6e8c5c01ef8b50631ea491db9279a13f3cb4e6
xpersonas <justin.neel@gmail.com>
2018-10-24 15:51:50 -0500

    fixing issue with checkbox being checked after invalid username/password submitted

--- js/simple_password_reveal.js
+++ js/simple_password_reveal.js
@@ -6,6 +6,8 @@
 (function ($, Drupal) {
   'use strict';
 
+  jQuery('#edit-simple-password-reveal-checkbox').prop( "checked", false );
+
   var selector = jQuery('input[data-drupal-selector^="edit-pass"]');
 
   selector.attr('type', 'text');
